6eba124371fb7ff6d16d531c45670a845a1ec817
Vishnu <vishnuvinod072@gmail.com>
2023-03-16 17:54:26 +0530

    feat: added alias import path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@hooks": "./src/hooks",
+            "@screens": "./src/screens",
+            "@components": "./src/components",
+            "@lib": "./src/lib",
+          },
+        },
+      ],
+    ],
   };
 };
