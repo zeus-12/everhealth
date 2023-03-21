60299ebeb02412b358cde7c6094e4e293753cad0
Vishnu <vishnuvinod072@gmail.com>
2023-03-21 14:16:57 +0530

    feat: update storage structure

--- src/types/storage.ts
+++ src/types/storage.ts
@@ -1,10 +1,27 @@
 export interface StorageType {
 	hasOnboarded: boolean;
+
 	user: {
+		isAuthenticated: boolean;
+		authDetails: {
+			provider: "google" | "facebook" | "email";
+			email: string;
+			photoURL: string;
+			displayName: string;			
+		}
+	}
+
+	personalDetails: {
 		name: string;
 		height: number;
 		weight: number;
-	};
+		age: number;
+	}
+
 	reminders: { startDate: Date; endDate: Date; time: string[]; task: string }[];
 	feedsInterested: string[];
+	appSettings: {
+		theme: "light" | "dark";
+		notifications: boolean;		
+	}
 }
